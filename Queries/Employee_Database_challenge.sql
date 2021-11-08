b431d681f2fa02156d8028da1bb66c0664f23b17
Gerardo Jiménez <88596274+Gerardoj09@users.noreply.github.com>
2021-11-08 00:13:45 -0600

    CHALLENGE 7 GJP

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -14,7 +14,6 @@ ON (ti.emp_no = em.emp_no)
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY em.emp_no;
 SELECT * FROM retirement_titles;
-
 -- Use Dictinct with Orderby to remove duplicate rows
 DROP TABLE unique_titles CASCADE;
 SELECT DISTINCT ON (emp_no) emp_no,
@@ -32,3 +31,23 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title;
 SELECT * FROM retiring_titles;
+
+
+--Deliverable 2: The Employees Eligible for the Mentorship Program
+DROP TABLE mentorship_eligibility CASCADE;
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+SELECT * FROM mentorship_eligibility;
